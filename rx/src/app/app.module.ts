4d491e6473a136adabdcc9ec30f6a09a051a974d
vg1ne <mbox696@gmail.com>
2017-07-21 19:36:08 +0300

    added custom-button.component to test rxjs with

--- rx/src/app/app.module.ts
+++ rx/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CustomButtonComponent } from './custom-button/custom-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ rx/src/app/custom-button/custom-button.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-button',
+  templateUrl: './custom-button.component.html',
+  styleUrls: ['./custom-button.component.css']
+})
+export class CustomButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  clicked(){
+    console.log('clicked')
+  }
+}
