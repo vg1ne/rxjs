31b0638cf530f0985ab29413a406f740b42200ff
vg1ne <mbox696@gmail.com>
2017-07-23 20:48:42 +0300

    added stream showing

--- rx/src/app/rx-interval/rx-interval.component.ts
+++ rx/src/app/rx-interval/rx-interval.component.ts
@@ -17,12 +17,19 @@ export class RxIntervalComponent implements OnInit {
       this.numbers.push(this.numbers.length)
     })
 
+    const fakeNumberArray = [1,2,3,4,5,6,7,8,9];
+
     const streamA = Observable.of(3,4,5,6)
     const streamB = streamA.map(i => i*3)
+    this.streamC = Observable.interval(2000)
+      .take(fakeNumberArray.length)
+      .map(i=>fakeNumberArray[i])
+
 
     streamB.subscribe(val => console.log(val))
 
 
   }
   numbers: Array<number> = []
+  streamC: Observable<number>
 }
