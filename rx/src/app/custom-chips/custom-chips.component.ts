ccb2eea1cb57cd214dcacfaaa561a1ca86914619
vg1ne <mbox696@gmail.com>
2017-07-23 15:34:29 +0300

    added timing to adding the result

--- rx/src/app/custom-chips/custom-chips.component.ts
+++ rx/src/app/custom-chips/custom-chips.component.ts
@@ -12,8 +12,15 @@ export class CustomChipsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.$chips = Observable.of(this.textStub).delay(5000)
+    //this.$chips = Observable.of(this.textStub).delay(3000)
+    this.$chips2 = Observable.interval(1500)
+      .take(this.textStub.length)
+      .map(i=>this.textStub[i])
+    this.$chips2.subscribe(x => this.chips.push(x))
   }
+
   $chips: Observable<Array<string>>
+  $chips2: Observable<string>
   textStub: Array<string> = ['one', 'two', 'three', 'four']
+  chips: Array<string> = []
 }
