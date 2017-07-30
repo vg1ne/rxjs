c4a02fb17445ff2dd8648fb20d05daa0f836e24f
vg1ne <mbox696@gmail.com>
2017-07-30 17:46:41 +0300

    operators

--- rx/src/app/app.component.ts
+++ rx/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs/Observable"
+import 'rxjs'
+import {OnInit} from "@angular/core"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  ngOnInit(){
+    this.$number = Observable
+      .from(this.numbers)
+      .take(this.numbers.length)
+      .map((x)=>x)
+      .filter((x, i)=>x%2>0);
+
+    const sub = this.$number
+      .delay(1000)
+      // .subscribe(x=> console.log(x))
+
+    const $more = Observable.of(1,2,3,4)
+
+    const foo = Observable.interval(1000).take(5)
+    const foo2 = Observable.of(10,11,12)
+    // const fooSub = $more
+    //   .startWith(1000000000000)
+    //   .concat(foo)
+    //   .take(5)
+    //   .subscribe(
+    //     x => console.log(x)
+    //   )
+    // const merged = foo.merge(foo2)
+    //   .subscribe(x=> console.log(x))
+    const interval = Observable.interval(500)
+    const foo3 = Observable.of(1,2,3,4,5)
+    const combined = Observable.zip(interval, foo3, (x,y)=> y)
+    combined.subscribe(x=>console.log(x))
+  }
+
+  private $number: Observable<number>
+  private numbers : Array<number> = [1,2,3,4,5,6,7]
 }
